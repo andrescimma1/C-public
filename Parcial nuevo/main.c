6d35c756c83f1f95c7b2fd741b4fa8d903a7cf78
andrescimma1 <andrescimma1@gmail.com>
2019-06-03 16:58:34 -0300

    Modificaciones parcial nuevo, falta del 4 al 12

--- Parcial nuevo/main.c
+++ Parcial nuevo/main.c
@@ -7,6 +7,7 @@
 #define TAMMAR 5
 #define TAMCOL 5
 #define TAMSER 4
+#define TAMTRA 10
 
 typedef struct
 {
@@ -46,6 +47,7 @@ typedef struct
 typedef struct
 {
     int id;
+    int estado;
     char patente[7];
     int idServicio;
     eFecha fecha;
@@ -63,6 +65,14 @@ void listarAuto(eAuto* pAuto, eMarca* listmarcas, int lenmarcas, eColor* listcol
 void listarServicios(eServicio* listservicios, int lenservicios);
 void bajaAuto(eAuto* listaut, int len, eMarca* listmarcas, int lenmarcas, eColor* listcolores, int lencolores);
 void modificarAuto(eAuto* listaut, int len, eMarca* listmarcas, int lenmarcas, eColor* listcolores, int lencolores);
+void inicializarTrabajos(eTrabajo* listtrabajos, int lentrabajos);
+int buscarTrabajoLibre(eTrabajo* listtrabajos, int lentrabajos);
+void altaTrabajo(eTrabajo* listtrabajos, int lentrabajos, eAuto* listaut, int len, eMarca* listmarcas, int lenmarcas, eColor* listcolores, int lencolores, eServicio* listservicios, int lenservicios);
+void listarTrabajo(eTrabajo* trabajos, eServicio* listservicios, int lenservicios);
+int listarTrabajos(eTrabajo* listtrabajos, int lentrabajos, eServicio* listservicios, int lenservicios);
+void mostrarAutosColorNegro(eAuto* listaut, int len, eMarca* listmarcas, int lenmarcas, eColor* listcolores, int lencolores);
+void mostrarAutosMarcaSeleccionada(eAuto* listaut, int len, eMarca* listmarcas, int lenmarcas, eColor* listcolores, int lencolores);
+void mostrarTrabajosAutoSeleccionado(eAuto* listaut, int len, eMarca* listmarcas, int lenmarcas, eColor* listcolores, int lencolores, eServicio* listservicios, int lenservicios, eTrabajo* listtrabajos, int lentrabajos);
 
 int main()
 {
@@ -92,6 +102,8 @@ int main()
 
     eAuto arrayAutos[TAMAUT];
     inicializarAutos(arrayAutos, TAMAUT);
+    eTrabajo arrayTrabajos[TAMTRA];
+    inicializarTrabajos(arrayTrabajos, TAMTRA);
 
     do
     {
@@ -126,11 +138,59 @@ int main()
                 system("pause");
                 break;
             case 'h':
-
+                altaTrabajo(arrayTrabajos, TAMTRA, arrayAutos, TAMAUT, arrayMarcas, TAMMAR, arrayColores, TAMCOL, arrayServicios, TAMSER);
                 system("pause");
                 break;
             case 'i':
-
+                listarTrabajos(arrayTrabajos, TAMTRA, arrayServicios, TAMSER);
+                system("pause");
+                break;
+            case '1':
+                mostrarAutosColorNegro(arrayAutos, TAMAUT, arrayMarcas, TAMMAR, arrayColores, TAMCOL);
+                system("pause");
+                break;
+            case '2':
+                mostrarAutosMarcaSeleccionada(arrayAutos, TAMAUT, arrayMarcas, TAMMAR, arrayColores, TAMCOL);
+                system("pause");
+                break;
+            case '3':
+                mostrarTrabajosAutoSeleccionado(arrayAutos, TAMAUT, arrayMarcas, TAMMAR, arrayColores, TAMCOL, arrayServicios, TAMSER, arrayTrabajos, TAMTRA);
+                system("pause");
+                break;
+            case '4':
+                listarTrabajos(arrayTrabajos, TAMTRA, arrayServicios, TAMSER);
+                system("pause");
+                break;
+            case '5':
+                listarTrabajos(arrayTrabajos, TAMTRA, arrayServicios, TAMSER);
+                system("pause");
+                break;
+            case '6':
+                listarTrabajos(arrayTrabajos, TAMTRA, arrayServicios, TAMSER);
+                system("pause");
+                break;
+            case '7':
+                listarTrabajos(arrayTrabajos, TAMTRA, arrayServicios, TAMSER);
+                system("pause");
+                break;
+            case '8':
+                listarTrabajos(arrayTrabajos, TAMTRA, arrayServicios, TAMSER);
+                system("pause");
+                break;
+            case '9':
+                listarTrabajos(arrayTrabajos, TAMTRA, arrayServicios, TAMSER);
+                system("pause");
+                break;
+            case '10':
+                listarTrabajos(arrayTrabajos, TAMTRA, arrayServicios, TAMSER);
+                system("pause");
+                break;
+            case '11':
+                listarTrabajos(arrayTrabajos, TAMTRA, arrayServicios, TAMSER);
+                system("pause");
+                break;
+            case '12':
+                listarTrabajos(arrayTrabajos, TAMTRA, arrayServicios, TAMSER);
                 system("pause");
                 break;
             default:
@@ -183,6 +243,19 @@ int menu()
     printf("G. Listar servicios.\n");
     printf("H. Alta trabajo.\n");
     printf("I. Listar trabajos.\n\n");
+
+    printf("1- Mostrar autos de color negro.\n");
+    printf("2- Mostrar autos de una marca seleccionada.\n");
+    printf("3- Mostrar todos los trabajos efectuados al auto seleccionado.\n");
+    printf("4- Listar los autos que no tuvieron trabajos.\n");
+    printf("5- Informar importe total de los trabajos realizados a un auto seleccionado.\n");
+    printf("6- Mostrar el servicio mas pedido.\n");
+    printf("7- Mostrar la recaudacion en una fecha en particular.\n");
+    printf("8- Mostrar todos los autos que realizaron un encerado y la fecha.\n");
+    printf("9- Trabajos realizados a autos blancos.\n");
+    printf("10- Facturacion total por los pulidos.\n");
+    printf("11- Informar la marca de autos que efectuaron mas lavados completos.\n");
+    printf("12- Listar los autos que recibieron trabajos en una fecha determinada.\n\n");
 
     printf("Ingrese una opcion: ");
     fflush(stdin);
@@ -307,6 +380,7 @@ void listarColores(eColor* listcolores, int lencolores)
     }
 }
 
+
 /** \brief Muestra un solo auto, pero se puede usar en la funcion listarAutos.
  *
  * \param int i, j;
@@ -338,6 +412,7 @@ void listarAuto(eAuto* pAuto, eMarca* listmarcas, int lenmarcas, eColor* listcol
             break;
         }
     }
+
 
     printf("%7s    %7s    %7s    %d\n", pAuto->patente, marcaEncontrada, colorEncontrado, pAuto->modelo);
 }
@@ -517,6 +592,314 @@ void modificarAuto(eAuto* listaut, int len, eMarca* listmarcas, int lenmarcas, e
             }
         }
     }
+}
+
+/** \brief Inicializa todos los trabajos en estado desocupado, y genera una id autoincremental.
+ *
+ * \param int i;
+ * \param
+ * \return No devuelve nada.
+ *
+ */
+
+void inicializarTrabajos(eTrabajo* listtrabajos, int lentrabajos)
+{
+    int i;
+
+    for(i=0; i<lentrabajos; i++)
+    {
+        listtrabajos[i].id = i;
+        listtrabajos[i].estado = 0;
+    }
+}
+
+/** \brief Busca un trabajo libre.
+ *
+ * \param int i;
+ * \param int indice;
+ * \return Devuelve el indice. Si es -1 significa que algo salio mal, si es 0 esta todo ok.
+ *
+ */
+
+int buscarTrabajoLibre(eTrabajo* listtrabajos, int lentrabajos)
+{
+    int i;
+    int indice = -1;
+
+    for(i=0; i<lentrabajos; i++)
+    {
+        if(listtrabajos[i].estado == 0)
+        {
+            indice = i;
+            break;
+        }
+    }
+
+    return indice;
+}
+
+/** \brief Da de alta un trabajo, pidiendo la patente y el servicio.
+ *
+ * \param int indice;
+ * \param
+ * \return No devuelve nada.
+ *
+ */
+
+void altaTrabajo(eTrabajo* listtrabajos, int lentrabajos, eAuto* listaut, int len, eMarca* listmarcas, int lenmarcas, eColor* listcolores, int lencolores, eServicio* listservicios, int lenservicios)
+{
+    int indice;
+
+    indice = buscarTrabajoLibre(listtrabajos, lentrabajos);
+
+    if(indice == -1)
+    {
+        printf("Error, no hay espacio.\n\n");
+    }
+    else
+    {
+        system("cls");
+
+        indice = listarAutos(listaut, len, listmarcas, lenmarcas, listcolores, lencolores);
+
+        if(indice == -1)
+        {
+            printf("\nNo se puede realizar la operacion\n\n");
+        }
+        else
+        {
+            printf("\n\nElija un auto por su patente: ");
+            fflush(stdin);
+            gets(listtrabajos[indice].patente);
+
+            int i;
+            int patenteValida = 0;
+            for(i=0; i<len; i++)
+            {
+                if(strcmp(listtrabajos[indice].patente, listaut[i].patente) == 0)
+                {
+                    patenteValida = 1;
+                    break;
+                }
+            }
+
+            if(patenteValida != 1)
+            {
+                printf("Error, patente ingresada no valida.\n\n");
+            }
+            else
+            {
+                listarServicios(listservicios, lenservicios);
+
+                printf("\n\nElija un servicio por su ID: ");
+                fflush(stdin);
+                scanf("%d", &listtrabajos[indice].idServicio);
+
+                int j;
+                int idServicioValido = 0;
+
+                for(j=0; j<lenservicios; j++)
+                {
+                    if(listtrabajos[indice].idServicio == listservicios[j].id)
+                    {
+                        idServicioValido = 1;
+                        break;
+                    }
+                }
+
+                if(idServicioValido != 1)
+                {
+                    printf("ID de servicio no valido. \n\n");
+                }
+                else
+                {
+                    printf("Ingrese la fecha dd/mm/aaaa: ");
+                    fflush(stdin);
+                    scanf("%d %d %d", &listtrabajos[indice].fecha.dia,
+                                      &listtrabajos[indice].fecha.mes,
+                                      &listtrabajos[indice].fecha.anio);
+
+                    listtrabajos[indice].estado = 1;
+                    printf("Datos ingresados con exito!!\n\n");
+                }
+
+            }
+        }
+    }
+}
 
 
+/** \brief Lista un solo trabajo.
+ *
+ * \param int i;
+ * \param char servicioEncontrado[25];
+ * \return No devuelve nada.
+ *
+ */
+
+void listarTrabajo(eTrabajo* trabajos, eServicio* listservicios, int lenservicios)
+{
+    int i;
+    char servicioEncontrado[25];
+
+    for(i=0; i<lenservicios; i++)
+    {
+        if(trabajos->idServicio == listservicios[i].id)
+        {
+            strcpy(servicioEncontrado, listservicios[i].descripcion);
+            break;
+        }
+    }
+
+    printf("%s    %s    %02d/%02d/%d\n", trabajos->patente, servicioEncontrado, trabajos->fecha.dia, trabajos->fecha.mes, trabajos->fecha.anio);
+}
+
+/** \brief Lista todos los trabajos que tengan el estado en 1.
+ *
+ * \param int i;
+ * \param int indice = -1;
+ * \return Devuelve -1 si no se encontraron trabajos, y sino todo ok.
+ *
+ */
+
+int listarTrabajos(eTrabajo* listtrabajos, int lentrabajos, eServicio* listservicios, int lenservicios)
+{
+    int i;
+    int indice = -1;
+
+    system("cls");
+
+    printf("LISTADO DE TRABAJOS\n");
+    printf("Patente   Servicio   Fecha\n");
+    printf("_______________________\n\n");
+
+    for(i=0; i<lentrabajos; i++)
+    {
+        if(listtrabajos[i].estado == 1)
+        {
+            listarTrabajo((listtrabajos + i), listservicios, lenservicios);
+            indice = i;
+        }
+    }
+
+    if(indice == -1)
+    {
+        printf("No se ha encontrado ningun trabajo\n\n");
+    }
+
+    return indice;
+}
+
+
+/** \brief Recorre todos los autos buscando que la idColor sea igual a 5000(negro) y
+ *          ademas que la id sea igual a 1(que el auto exista)
+ * \param int i;
+ * \param int indice = -1;
+ * \return No devuelve nada.
+ *
+ */
+
+void mostrarAutosColorNegro(eAuto* listaut, int len, eMarca* listmarcas, int lenmarcas, eColor* listcolores, int lencolores)
+{
+    int i;
+    int indice = -1;
+
+    system("cls");
+
+    printf("LISTADO DE AUTOS DE COLOR NEGRO\n");
+    printf("_______________________________\n");
+
+    for(i=0; i<len; i++)
+    {
+        if(listaut[i].idColor == 5000 && listaut[i].id == 1)
+        {
+            listarAuto((listaut + i), listmarcas, lenmarcas, listcolores, lencolores);
+            indice = i;
+        }
+    }
+
+    if(indice == -1)
+    {
+        printf("No hay autos de color negro.\n\n");
+    }
+}
+
+
+/** \brief Busca los autos que tengan la idMarca igual a la ID de la marca ingresada y
+ *          tengan el estado en 1(que el auto exista).
+ * \param  int i;
+ * \param  int indice = -1;
+ * \param  int marcaSeleccionada;
+ * \return No devuelve nada.
+ *
+ */
+
+void mostrarAutosMarcaSeleccionada(eAuto* listaut, int len, eMarca* listmarcas, int lenmarcas, eColor* listcolores, int lencolores)
+{
+    int i;
+    int indice = -1;
+    int marcaSeleccionada;
+
+    system("cls");
+
+    listarMarcas(listmarcas, lenmarcas);
+
+    printf("\n\nIngrese ID de la marca: ");
+    fflush(stdin);
+    scanf("%d", &marcaSeleccionada);
+
+    system("cls");
+
+    printf("LISTADO DE AUTOS DE LA MARCA SELECCIONADA\n");
+    printf("_________________________________________\n");
+
+    for(i=0; i<len; i++)
+    {
+        if(marcaSeleccionada == listaut[i].idMarca && listaut[i].id == 1)
+        {
+            listarAuto((listaut + i), listmarcas, lenmarcas, listcolores, lencolores);
+            indice = i;
+        }
+    }
+
+    if(indice == -1)
+    {
+        printf("No hay autos de la marca ingresada para mostrar.\n\n");
+    }
+}
+
+
+void mostrarTrabajosAutoSeleccionado(eAuto* listaut, int len, eMarca* listmarcas, int lenmarcas, eColor* listcolores, int lencolores, eServicio* listservicios, int lenservicios, eTrabajo* listtrabajos, int lentrabajos)
+{
+    int i;
+    int indice = -1;
+    int indice2 = -1;
+    char patenteIngresada[7];
+
+    system("cls");
+
+    indice = listarAutos(listaut, len, listmarcas, lenmarcas, listcolores, lencolores);
+
+    if(indice != -1)
+    {
+        printf("Seleccione un auto por su patente: ");
+        fflush(stdin);
+        gets(patenteIngresada);
+
+        printf("\nLISTADO DE TRABAJOS DEL AUTO SELECCIONADO\n");
+        printf("_________________________________________\n");
+        for(i=0; i<lentrabajos; i++)
+        {
+            if(strcmp(patenteIngresada, listtrabajos[i].patente) == 0 && listtrabajos[i].estado == 1)
+            {
+                listarTrabajo((listtrabajos + i), listservicios, lenservicios);
+                indice2 = i;
+            }
+        }
+    }
+
+    if(indice2 == -1)
+    {
+        printf("No hay trabajos que mostrar.\n\n");
+    }
 }
