eba61b3e0e196f635da1b6a250aa339235775d1d
andrescimma1 <andrescimma1@gmail.com>
2019-06-17 17:55:13 -0300

    Ejercicio 1 clase 20

--- Ejercicio clase 20/main.c
+++ Ejercicio clase 20/main.c
@@ -2,49 +2,55 @@
 #include <stdlib.h>
 #include <string.h>
 
-typedef struct
+struct S_Person
 {
-    char nombre[32];
-    int edad;
-}Persona;
+    char name[20];
+    int age;
+};
+typedef struct S_Person Person;
+
+int enterPerson(Person* p);
+void printPerson(Person* p);
 
 int main()
 {
-    Persona personas[5];
-    int indice = 0;
-    /*
-    printf("Ingrese nombre: ");
-    gets(personas[indice].nombre);
-    while(strcmp(personas[indice].nombre, "salir") == 0)
+    Person p;
+
+    while(enterPerson(&p) == 0)
     {
-        printf("Ingrese nombre: ");
-        gets(personas[indice].nombre);
+        printPerson(&p);
     }
-    printf("Ingrese edad: ");
-    scanf("%d", &personas[indice].edad);
 
-    printf("%s   %d\n", personas[indice].nombre, personas[indice].edad);
-    */
-
-    do
-    {
-        printf("Ingrese nombre: ");
-        gets(personas[indice].nombre);
-        if(strcmp(personas[indice].nombre, "salir") != 0)
-        {
-            printf("Ingrese edad: ");
-            scanf("%d", &personas[indice].edad);
-            indice++;
-        }
-    }while(strcmp(personas[indice].nombre, "salir") != 0);
-
-    int i;
-    for(i=0; i<5; i++)
-    {
-        if(strcmp(personas[i].nombre, "salir") != 0)
-        {
-            printf("%s   %d\n", personas[i].nombre, personas[i].edad);
-        }
-    }
+    system("pause");
     return 0;
+}
+
+
+/** \brief Pide al usuario los datos de una persona y los almacena en la struct
+ * \param Person* p puntero a tipo Person donde se guardarán los datos
+ * \return 1(si se ingresa salir) o 0(de lo contrario)
+ */
+int enterPerson(Person* p)
+{
+    int index = 0;
+    printf("Ingrese el nombre: ");
+    scanf("%s", p->name);
+    if(strncmp(p->name, "salir", 5) == 0)
+    {
+        return 1;
+    }
+    printf("Ingrese la edad: ");
+    scanf("%d", &(p->age));
+    return 0;
+}
+
+
+/** \brief Imprime los datos de una persona
+ * \param Person* p puntero a tipo Person donde se guardarán los datos
+ * \return void
+ *
+ */
+void printPerson(Person* p)
+{
+    printf("Nombre:\t%s \tEdad:%d\r\n", p->name, p->age);
 }
